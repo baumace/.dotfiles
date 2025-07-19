bc828b0c70c6be761453bf85da81af20db5a0584
Ace Baum <jaybaum.03@gmail.com>
2025-07-19 19:32:25 -0400

    zsh import

---
+++ zsh/custom/aliases.zsh
@@ -0,0 +1,2 @@
+# Custom git extensions
+source "${0:A:h}/aliases/gitx.zsh"
---
+++ zsh/custom/aliases/gitx.zsh
@@ -0,0 +1,30 @@
+# gitx-cb: Clone a bare repo, fix fetch config, and add a main branch worktree
+# Usage: gitx-cb <remote-url>
+# Example: gitx-cb git@github.com:user/repo.git
+gitx-cb() {
+  # pull args
+  local remote_url=$1
+  local repo_name=$(basename "$remote_url" .git)
+  local repo_dir=$(basename "$remote_url")
+
+  # clean
+  rm -rf "$repo_name"
+  mkdir "$repo_name"
+  cd "$repo_name"
+
+  # clone
+  git clone --bare "$remote_url"
+
+  # fetch
+  git --git-dir="$repo_dir" config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+  git --git-dir="$repo_dir" fetch
+
+  # add main worktree
+  git --git-dir="$repo_dir" worktree add main main
+
+  # set upstream branch
+  cd "$repo_name/main"
+  git branch --set-upstream-to origin/main
+
+  cd ..
+}
